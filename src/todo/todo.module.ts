ed1e01c8010fb1f8d25245d7054b32e4cfa19b90
Samiksha Shinde <samikshashinde142002@gmail.com>
2024-07-21 00:03:24 +0530

    Describe your changes

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [TodoController],
   providers: [TodoService],
 })
