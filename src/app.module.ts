0bf030d7fd6c1395e16cbcce2649f61c6cc24707
Samiksha Shinde <samikshashinde142002@gmail.com>
2024-07-20 22:29:43 +0530

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { TodoModule } from './todo/todo.module';
+import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+
+
+@Module({
+  imports: [DatabaseModule, TodoModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
