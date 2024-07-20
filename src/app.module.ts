01457d2ccfcc0bf85c8d42cef1803a5aa75b87ab
Samiksha Shinde <samikshashinde142002@gmail.com>
2024-07-20 23:11:04 +0530

    Commit message

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,6 @@ import { TodoModule } from './todo/todo.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-
 
 @Module({
   imports: [DatabaseModule, TodoModule, AuthModule],
