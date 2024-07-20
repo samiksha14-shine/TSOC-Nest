a87f6ade49067a4d579c33e62a6ee5cedb0d55ef
Samiksha Shinde <samikshashinde142002@gmail.com>
2024-07-21 02:25:14 +0530

    vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -6,25 +6,59 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
-
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe());
+
   const config = new DocumentBuilder()
-  .setTitle('Todo REST API')
-  .setDescription("A rest api to list user's todos/tasks, authorization implemented using JWT token. THe API is made using NestJs.")
-  .setVersion('1.0')
-  .addBearerAuth()
-  .build();
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('/swagger', app, document, {
-    customCssUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    .setTitle('Todo REST API')
+    .setDescription("A REST API to list user's todos/tasks, with authorization implemented using JWT token. The API is made using NestJS.")
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  
+  // Set up Swagger UI at /swagger
+  SwaggerModule.setup('/swagger', app, document, {
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
     customJs: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
     ],
-});
+  });
+
   await app.listen(3000);
 }
 bootstrap();
+
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+
+
+//   app.enableCors();
+//   app.useGlobalPipes(new ValidationPipe())
+//   const config = new DocumentBuilder()
+//   .setTitle('Todo REST API')
+//   .setDescription("A rest api to list user's todos/tasks, authorization implemented using JWT token. THe API is made using NestJs.")
+//   .setVersion('1.0')
+//   .addBearerAuth()
+//   .build();
+// const document = SwaggerModule.createDocument(app, config);
+// SwaggerModule.setup('/swagger', app, document, {
+//     customCssUrl:
+//       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+//     customJs: [
+//       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+//       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+//     ],
+// });
+//   await app.listen(3000);
+// }
+// bootstrap();
